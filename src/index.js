b8545225394cfec4c2ec782d91552f20b46e1764
Alexey Yakauleu MBP <yakovlev@yakovlevdev.com>
2024-08-18 15:34:34 +0300

    feat: end with task reverse int

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 module.exports = function reverse (n) {
-    let toString = n.toString();
+    let toString = Math.abs(n).toString(); // get number without operators to string
     let array = toString.split('');
     let reverseArray = array.reverse(); //need get integer from array in next step!
-    return reverseArray;
+    let reverseString = reverseArray.join(''); //get string from array
+    return Number(reverseString); // get integer from string
 }
